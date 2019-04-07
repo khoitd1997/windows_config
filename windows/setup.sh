dac85bf983a7ac74140a9157dc16931eb9fc829e
khoitd1997 <khoidinhtrinh@gmail.com>
2019-04-07 14:56:01 -0700

    fix: Changed link and vscode steps

--- windows/setup.sh
+++ windows/setup.sh
@@ -10,16 +10,14 @@ cd ${currDir}
 
 choco install -Y ${choco_app_list}
 
-# TODO: add scoop app list if possible
-
 # symlink on git bash is weird
-cp -v cmder/ConEmu.xml /c/tools/Cmder/vendor/conemu-maximus5/ConEmu.xml
+ln -sfv ${currDir}/cmder/ConEmu.xml /c/tools/Cmder/vendor/conemu-maximus5/ConEmu.xml
 
 git submodule init
 git submodule update
 
 # vscode stuffs
 bash ../fedora_kd/userland/vscode/vscode_extension.sh
+bash ../fedora_kd/userland/vscode/vscode_configure.sh
 # replace fonts for windows
 sed -ie 's:source code pro:Consolas:g' $APPDATA/Code/User/settings.json
-bash ../fedora_kd/userland/vscode/vscode_configure.sh
